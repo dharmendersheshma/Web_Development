c36ffae5f09a6bc824eb3cbaf39bbaeae5672192
dharmendersheshma <dharmendra.shesma@gmail.com>
2020-08-01 19:32:30 +0530

    Add Netflix frontend

---
+++ netflix/build/precache-manifest.05c2f96c61cc077f4831271b50c288c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebcf3c7dffc31afa693675faff54442",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ebc379d6a9ad338440",
+    "url": "/static/css/main.70cca306.chunk.css"
+  },
+  {
+    "revision": "6d3dbe0d3dfdd72adbe5",
+    "url": "/static/js/2.0231de7a.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.0231de7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ebc379d6a9ad338440",
+    "url": "/static/js/main.be67ab8f.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  }
+]);
